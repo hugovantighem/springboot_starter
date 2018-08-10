ea5526b053e6fbc6afea521a11a6f5f90fad70d2
hvantighem <hugo.vantighem@gmail.com>
2018-08-10 17:38:55 +0200

    cors

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # springboot_starter
+
+
+curl -i  -d "username=user&password=test"  http://localhost:8080/login
+curl -i  -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyIiwiaXNzIjoiYXV0aC1zZXJ2aWNlLWp3dCIsImlhdCI6MTUzMzg5MzI3NiwiZXhwIjoxNTM0NDk4MDc2LCJyb2xlcyI6IlJPTEVfVVNFUiJ9.wnIsKi4uEqohe7XZBT71H_uMXXa9Y0eLc69CMG_PLDQ"  http://localhost:8080/api/ping
+
+
+## CORS
+
+Cors are supported, a simple test is shown with ajax into static/index.html
+
+```
+cd static && python -m SimpleHTTPServer
+```
+
+Then browse to [http://localhost:8000](http://localhost:8000)
--- src/main/java/com/config/SecurityConfig.java
+++ src/main/java/com/config/SecurityConfig.java
@@ -20,6 +20,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -60,7 +65,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.cors().configurationSource(corsConfigurationSource()).and().authorizeRequests()
                 .antMatchers(AUTHORIZED_LIST).permitAll()
                 .antMatchers("/api/**").authenticated()
                 .and().formLogin()
@@ -88,6 +93,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         authProvider.setUserDetailsService(userDetailsService);
         authProvider.setPasswordEncoder(encoder());
         return authProvider;
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+        configuration.setAllowCredentials(true);
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setExposedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
